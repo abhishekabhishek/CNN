b4e6290f95c28715cc88e0a3be0e53bc75ef401a
Julian Ding <scorpiusa51@gmail.com>
2019-05-09 16:59:21 -0700

    Fixed small bugs in main .py and added --help output

--- USER/main.py
+++ USER/main.py
@@ -16,35 +16,43 @@ if par_dir not in sys.path:
     
 # Let's begin...
 import training_utils.engine as net
-import models
+import models.resnet as resnet
 import argparse
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-gpu', nargs='+', dest='gpu', required=False)
-    parser.add_argument('-path', dest='path', required=True)
-    parser.add_argument('-vs', dest='val_split', required=True)
-    parser.add_argument('-ts', dest='test_split', required=True)
-    parser.add_argument('-tnb', dest='batch_size_train', required=True)
-    parser.add_argument('-vlb', dest='batch_size_val', required=True)
-    parser.add_argument('-tsb', dest='batch_size_test', required=True)
-    parser.add_argument('-tsb', dest='batch_size_test', required=True)
-    parser.add_argument('-save', dest='save_path', required=False)
-    parser.add_argument('-desc', dest='data_description', required=False)
+    parser = argparse.ArgumentParser(
+            description= '[HK-Canada] TRIUMF Neutrino Group: Welcome to the deep learning interface! Collaborators: Wojciech Fedorko, Julian Ding, Abhishek Kajal',
+            epilog= 'Happy training!')
+    parser.add_argument('-device', dest='device', default='cpu',
+                        required=False, help='Enter cpu to use CPU resources or gpu to use GPU resources.')
+    parser.add_argument('-gpu', nargs='+', dest='gpu_list', default=None,
+                        required=False, help='List of available GPUs')
+    parser.add_argument('-path', dest='path',
+                        required=True, help='Path to training dataset.')
+    parser.add_argument('-vs', dest='val_split',
+                        required=True, help='Fraction of dataset used in validation. Note: requires vs + ts < 1')
+    parser.add_argument('-ts', dest='test_split',
+                        required=True, help='Fraction of dataset used in testing. Note: requires vs + ts < 1')
+    parser.add_argument('-tnb', dest='batch_size_train',
+                        required=True, help='Batch size for training.')
+    parser.add_argument('-vlb', dest='batch_size_val',
+                        required=True, help='Batch size for validating.')
+    parser.add_argument('-tsb', dest='batch_size_test',
+                        required=True, help='Batch size for testing.')
+    parser.add_argument('-save', dest='save_path', default='save_path',
+                        required=False, help='Specify path to save data to. Default is save_path')
+    parser.add_argument('-desc', dest='data_description', default='DATA DESCRIPTION',
+                        required=False, help='Specify description for data.')
     
     config = parser.parse_args()
-    if config.gpu is None:
+    if config.gpu_list is None:
         config.gpu = False
-    if config.save_path is None:
-        config.save_path = 'save_path'
-    if config.data_description is None:
-        config.data_description = 'DATA DESCRIPTION'
         
     return config
 
 if __name__ == '__main__':
     config = main()
-    model = models.resnet.resnet18()
+    model = resnet.resnet18()
     nnet = net.Engine(model, config)
     nnet.train()
     
